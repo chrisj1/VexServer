f033f5ed813d20c0e436b49c4264339e70c23a8e
Christopher J. Jerrett <cjjerrett@l-mbpr13662.stjohnsprep.org>
2017-08-09 16:10:42 -0400

    json api fetching

--- api/models.py
+++ api/models.py
@@ -8,10 +8,10 @@ from api.jsonHelpers import get_json_from_URL
 # Create your models here.
 class Team(models.Model):
     team_number = models.CharField(max_length=10)
-    sku = models.CharField(max_length=70)
-    organization = models.CharField(max_length=70)
+    organisation = models.CharField(max_length=70)
     team_name = models.CharField(max_length=70)
     robot_name = models.CharField(max_length=100)
+    grade = models.BooleanField()
 
     def update(self):
         json = get_json_from_URL('https://api.vexdb.io/v1/get_teams?team=9228A')
@@ -20,11 +20,16 @@ class Team(models.Model):
             return
         print(json['size'])
         result = json['result'][0]
-        print(result[0])
-        self.organization = result['organization']
+        self.organisation = result['organisation']
         self.team_name = result['team_name']
         self.robot_name = result['robot_name']
+        self.grade = result['grade'] == 'High School'
         self.save()
+
+
+    def __str__(self):
+        return "team_number: %s, organisation: %s, team_name: %s, robot_name %s, grade %s".format(
+            self.team_number, self.organisation, self.team_name, self.robot_name, self.grade)
 
 
 class Match(models.Model):
@@ -38,9 +43,9 @@ class Match(models.Model):
 
     time = models.DateTimeField()
 
-
 class IndivualTeamMatchPerformance(models.Model):
     team = models.ForeignKey(Team)
+    match = models.ForeignKey(Match)
 
     teleop_description = models.CharField(max_length=700)
     cones_on_goal = models.IntegerField()
